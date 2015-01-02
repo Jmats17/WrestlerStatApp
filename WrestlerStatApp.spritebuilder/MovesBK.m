4e4530800ab3b1ff8d48d887972360b48ba2d729
Jmats17 <Justin@Justins-Air.home>
2015-01-02 18:22:24 -0500

    got the stats to work

--- WrestlerStatApp.spritebuilder/MovesBK.m
+++ WrestlerStatApp.spritebuilder/MovesBK.m
@@ -9,6 +9,19 @@
 #import "MovesBK.h"
 
 @implementation MovesBK
+{
+    CCButton *_yes;
+    CCButton *_no;
+    CCLabelTTF * _correct;
+    CCLabelTTF * _total;
+    CCLabelTTF *_percent;
+    float percentTotal;
+    float total;
+    float correct;
+    BOOL clicked;
+}
+
+
 
 
 -(void)back
@@ -19,4 +32,39 @@
     [[CCDirector sharedDirector] replaceScene:back];
     
 }
+
+-(void)no {
+    total++;
+    _correct.string = [NSString stringWithFormat:@"%.0f", correct];
+    _correct.visible = true;
+    _total.string = [NSString stringWithFormat:@"/ %.0f", total];
+    _total.visible = true;
+    percentTotal = (int)((correct / total) * 100);
+    _percent.string = [NSString stringWithFormat:@"Success Rate: %.0f %%", percentTotal];
+
+}
+
+-(void)yes {
+    total++;
+    correct++;
+    _total.string = [NSString stringWithFormat:@"/ %.0f", total];
+    _total.visible = true;
+    _correct.string = [NSString stringWithFormat:@"%.0f", correct];
+    _correct.visible = true;
+    percentTotal = (int)((correct / total) * 100);
+    _percent.string = [NSString stringWithFormat:@"Success Rate: %.0f %%", percentTotal];
+
+}
+
+//-(void)percentTotal {
+//    percentTotal = (int)((correct / total) * 100);
+//    _percent.string = [NSString stringWithFormat:@"%f", percentTotal];
+//}
+
+-(void)test {
+
+}
+
+
+
 @end
